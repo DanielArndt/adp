dbd5ef9ffeaf3d910b9b4a0693ac6943743fba86
Daniel Arndt <darndt@cs.dal.ca>
2010-12-08 14:23:15 -0400

    Fixed header to include my personal URL and the google code URL.

--- adp.go
+++ adp.go
@@ -15,6 +15,13 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information please visit my website at:
+ * http://web.cs.dal.ca/~darndt
+ *
+ * Or the code's repository:
+ *
+ * http://code.google.com/p/adp
  *  
  */
 
